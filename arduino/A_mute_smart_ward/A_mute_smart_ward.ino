de05f7e1aa5cc70ade5dcf836eaf50d22b39db87
Angelus123 <izerefaifelix@gmail.com>
2021-10-09 22:07:32 +0200

    Add servo file

--- arduino/A_mute_smart_ward/A_mute_smart_ward.ino
+++ arduino/A_mute_smart_ward/A_mute_smart_ward.ino
@@ -1,14 +1,26 @@
+#include <Servo.h>
 #define numOfValsRec 5
 #define digitsPerValRec 1
 int valsRec[numOfValsRec];
+
+Servo servoThumb;
+Servo servoIndex;
+Servo servoMiddle;
+Servo servoRing;
+Servo servoPinky;
+
 int stringLength = numOfValsRec*digitsPerValRec + 1;
 int counter = 0;
 bool counterStart = false ;
 String receivedString;
 
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(9600);
+servoThumb.attach(7);
+servoIndex.attach(9); 
+servoMiddle.attach(11);
+servoRing.attach(8);
+servoPinky.attach(10);
 
 }
 void receivedData(){
@@ -39,6 +51,10 @@ void receivedData(){
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ receivedData();
+ if (valsRec[0] == 1){servoThumb.write(180);}else{servoThumb.write(0); }
+ if (valsRec[1] == 1){servoIndex.write(180);}else{servoIndex.write(0); }
+ if (valsRec[2] == 1){servoMiddle.write(180);}else{servoMiddle.write(0); }
+ if (valsRec[3] == 1){servoRing.write(180);}else{servoRing.write(0); }
+ if (valsRec[4] == 1){servoPinky.write(180);}else{servoPinky.write(0); }
 }
