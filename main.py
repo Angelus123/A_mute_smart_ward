aa326ec608baef0cd8d1ae43d7ec68964d5df788
Angelus123 <izerefaifelix@gmail.com>
2021-10-12 10:11:46 +0200

    Find hand and its landmarks

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-
+from cvzone.HandTrackingModule import HandDetector
 import cv2
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 while True:
+    # Get image frame
     success, img = cap.read()
-    cv2.imshow('Image', img)
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
